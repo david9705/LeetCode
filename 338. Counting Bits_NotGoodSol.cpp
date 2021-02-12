3397548f9fd3eda8af57ce3829c0cbc3293650c1
david9705 <abc88662a@gmail.com>
2021-02-12 16:17:26 +0000

    Leetcode_338(?)

---
+++ 338. Counting Bits_NotGoodSol.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        
+        vector<int> ans;
+        
+        for(int i = 0 ;i <= num; i ++)
+        {
+            ans.push_back(bitset<32>(i).count());
+        }
+        
+    
+        return ans;
+        
+    }
+};
